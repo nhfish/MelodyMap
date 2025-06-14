df34204fee5cd92272eb93bff3350bdf85054ebf
nf <nf>
2025-06-14 08:50:27 -0700

    changes from local

---
+++ MelodyMap/MelodyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelodyMapApp.swift
+//  MelodyMap
+//
+//  Created by Nathan Fisher on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MelodyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
