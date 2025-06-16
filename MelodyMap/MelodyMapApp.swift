99c3f66677a3b401f4dbc4caa894efa49f1c9b16
nathan <nathan.h.fisher@gmail.com>
2025-06-15 23:12:53 -0700

    mapapp updated pushed to try and debug

--- MelodyMap/MelodyMapApp.swift
+++ MelodyMap/MelodyMapApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct MelodyMapApp: App {
+    // Single shared instances
+    @StateObject private var purchaseService = PurchaseService()
+    @StateObject private var usageService    = UsageTrackerService()
+    @StateObject private var adService       = AdService()
+
     var body: some Scene {
         WindowGroup {
             MainTabView()
+                .environmentObject(purchaseService)
+                .environmentObject(usageService)
+                .environmentObject(adService)
         }
     }
 }
